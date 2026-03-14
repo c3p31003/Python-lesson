5a43365c881a9fe1084ad0b2ae8ce2e872ba4a21
c3p31003 <c3p31003@bunkyo.ac.jp>
2026-03-14 21:49:23 +0900

    create desktop app

---
+++ section1/bonus/bonus16.py
@@ -0,0 +1,17 @@
+import FreeSimpleGUI as sg
+
+label1 = sg.Text("Select files to compress:")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose")
+label2 = sg.Text("Select files to compress:")
+input2 = sg.Input()
+cb2 = sg.FolderBrowse("Choose")
+
+compress_button = sg.Button("Compress")
+
+window = sg.Window('App',
+                    layout=[[label1,input1,choose_button1],
+                    [label2,input2,cb2],
+                    [compress_button]])
+window.read()
+window.close()
