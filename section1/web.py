822cb534566a0b98f3c2640dba7bb42e050a88c9
c3p31003 <c3p31003@bunkyo.ac.jp>
2026-06-13 22:10:01 +0900

    Implement "Add new todo items" feature

--- section1/web.py
+++ section1/web.py
@@ -3,6 +3,14 @@ from modules import functions
 
 #url http://localhost:8501/
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todo_list = functions.get_todos()
+    todo_list.append(todo)
+    functions.write_todos(todo_list)
+    st.session_state["new_todo"]=""
+
+
 todos = functions.get_todos()
 
 #title function returns a title instance.
@@ -12,13 +20,13 @@ st.write("This app is to increase your productivity.")
 
 for todo in todos:
     st.checkbox(todo)
-# st.checkbox("Buy grocery")
-# st.checkbox("Throw the trash.")
 
 
-new_todo = st.text_input(label="", placeholder="Add new todo... ")
-todos = functions.get_todos()
-todos.append(new_todo)
 
-functions.write_todos(todos)
+st.text_input(label="", placeholder="Add new todo... ", 
+                         on_change=add_todo, key='new_todo')
+
+st.session_state
+
+
     
