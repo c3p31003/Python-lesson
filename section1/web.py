7a6cd1b5ad8e0a3f4e840857cf93fb590844ef08
c3p31003 <c3p31003@bunkyo.ac.jp>
2026-06-14 19:45:09 +0900

    Fix list update on todo removal

--- section1/web.py
+++ section1/web.py
@@ -3,6 +3,10 @@ from modules import functions
 
 #url http://localhost:8501/
 
+#アプリ実行コマンド streamlit run web.py
+
+#Streamlitは、何かが操作されるたびに、Pythonスクリプトを上から下まで実行する
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todo_list = functions.get_todos()
@@ -15,18 +19,25 @@ todos = functions.get_todos()
 
 #title function returns a title instance.
 st.title("My Todo App")
-st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
-
-for todo in todos:
-    st.checkbox(todo)
+# st.subheader("This is my todo app.")
+# st.write("This app is to increase your productivity.")
 
 
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo[:-1])
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        st.rerun()
+      
 
-st.text_input(label="", placeholder="Add new todo... ", 
+
+
+
+st.text_input(label="Add new todo", placeholder="Add new todo... ", 
                          on_change=add_todo, key='new_todo')
 
-st.session_state
+# st.session_state
 
 
     
